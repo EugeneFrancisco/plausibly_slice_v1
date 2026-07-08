33a73f883b69aa2dd6c0155ab0fc02027fc0ae1d
hegreamo <hegreamo@gmail.com>
2026-07-08 14:41:12 -0700

    Added Personal Files

---
+++ test.py
@@ -0,0 +1,15 @@
+load('/Users/henrigreamo/Desktop/Sliceness of Knots Code Projects/rbg.py')
+load('/Users/henrigreamo/Desktop/Sliceness of Knots Code Projects/find_0_friends.py')
+
+'''
+Test original RBG link is_super_special code:
+
+blue_ex=snappy.Manifold('K11n34')
+ans=find_common_zero_surgery_via_words(blue_ex,3)
+green_ex=snappy.Manifold(ans[0][3])
+forms_super_special_RBG_link(blue_ex,green_ex)
+'''
+
+
+
+
