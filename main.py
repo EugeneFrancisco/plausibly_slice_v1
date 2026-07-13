95df0564753217dec9b646a69539d2bfefd3996c
Eugene Francisco <eugjavfra01@gmail.com>
2026-07-13 10:44:59 -0700

    main script added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from src.knot import Knot
+from tqdm import tqdm
+
+knot = Knot("K11n34")
+friends = knot.find_n_friends(1)
+
+count = 0
+for friend in tqdm(friends):
+    identifier = friend[3]
+    knot_friend = Knot(identifier)
+    link = Knot.search_for_n_rbg_link(knot, knot_friend, 1)
+    if link is not None:
+        print("yay")
